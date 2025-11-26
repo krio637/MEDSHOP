70be8b637a783808355faf4509cdad34f7634f76
krio367 <aj9285696@gmail.com>
2025-11-26 14:40:32 +0530

    Simplified database config to use DATABASE_URL

--- medshop/settings.py
+++ medshop/settings.py
@@ -79,9 +79,7 @@ WSGI_APPLICATION = 'medshop.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,
-        conn_health_checks=True,
+        conn_max_age=600
     )
 }
 
