58d123e4417e2b38cb26a44e55e8472e30d6b8f1
krio637 <aj9285696@gmail.com>
2025-11-26 15:11:47 +0530

    Fix migration conflicts in build script
    Added a step to fix migration conflicts before running migrations.

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ pip install -r requirements.txt
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
+echo "Fixing migration conflicts..."
+# Prevent "column image already exists" error
+python manage.py migrate medicines 0003 --fake || true
+
 echo "Running migrations..."
 python manage.py migrate --no-input
 
